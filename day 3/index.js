6020ea39bad7377f522218840bea30d42395d50d
chae-chae <54352086+chae-chae@users.noreply.github.com>
2020-01-31 16:04:35 +0900

    vanilla js study

---
+++ day 3/index.js
@@ -0,0 +1,8 @@
+//const title = document.getElementById("title");
+const title = document.querySelector("#title");
+
+function handleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleClick);
---
+++ day 4/index.js
@@ -0,0 +1,32 @@
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  console.log(currentColor);
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+
+init();
+
+function handleOffline() {
+  console.log("no WIFI");
+}
+
+function handleOnline() {
+  console.log("welcome back");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
